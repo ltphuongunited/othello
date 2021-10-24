f99e3877f0a249643156b3fca10b4d27cc04b69c
ltphuongunited <68008652+ltphuongunited@users.noreply.github.com>
2021-10-24 10:13:04 +0700

    third

--- reversiai.py
+++ reversiai.py
@@ -10,7 +10,7 @@ class ReversiAI():
     # @staticmethod
     def get_next_move(self, board, player):
         # the depth argument defines how many levels deep we go before using heuristic
-        _, move = self.alpha_beta(board, 3, player, player)
+        _, move = self.minimax(board, 3, player, player)
         return move
     # @staticmethod
     def minimax(self, board, depth, player, MAX_PLAYER):
@@ -36,10 +36,10 @@ class ReversiAI():
         # if it is a min node
         else:
             best_value = AIHelper.INFINITY
-            available_moves = helper.available_moves(board, -MAX_PLAYER)
+            available_moves = helper.available_moves(board, MAX_PLAYER)
             for move in available_moves:
-                node = helper.get_resulting_board(board, -MAX_PLAYER, move)
-                value, _ = self.minimax(node, depth - 1, player, MAX_PLAYER)
+                node = helper.get_resulting_board(board, MAX_PLAYER, move)
+                value, _ = self.minimax(node, depth - 1, player, -MAX_PLAYER)
                 if value < best_value:
                     best_value = value
                     best_move = move
@@ -72,10 +72,10 @@ class ReversiAI():
 
         # if it is a min node
         else:
-            available_moves = helper.available_moves(board, -MAX_PLAYER)
+            available_moves = helper.available_moves(board, MAX_PLAYER)
             for move in available_moves:
-                node = helper.get_resulting_board(board, -MAX_PLAYER, move)
-                value, _ = self.alpha_beta_pruning(node, depth - 1, player, MAX_PLAYER, a, b)
+                node = helper.get_resulting_board(board, MAX_PLAYER, move)
+                value, _ = self.alpha_beta_pruning(node, depth - 1, player, -MAX_PLAYER, a, b)
                 if value < b:
                     b = value
                     best_move = move
@@ -107,15 +107,25 @@ class ReversiAI():
         Y1 = [0, 1, 1, 1, 0, -1, -1, -1]
 
         # wondering where this came from? check the link in the github ripo from University of Washington
+        # V = [
+        #     [20, -3, 11, 8, 8, 11, -3, 20],
+        #     [-3, -7, -4, 1, 1, -4, -7, -3],
+        #     [11, -4, 2, 2, 2, 2, -4, 11],
+        #     [8, 1, 2, -3, -3, 2, 1, 8],
+        #     [8, 1, 2, -3, -3, 2, 1, 8],
+        #     [11, -4, 2, 2, 2, 2, -4, 11],
+        #     [-3, -7, -4, 1, 1, -4, -7, -3],
+        #     [20, -3, 11, 8, 8, 11, -3, 20]
+        # ]
         V = [
-            [20, -3, 11, 8, 8, 11, -3, 20],
-            [-3, -7, -4, 1, 1, -4, -7, -3],
-            [11, -4, 2, 2, 2, 2, -4, 11],
-            [8, 1, 2, -3, -3, 2, 1, 8],
-            [8, 1, 2, -3, -3, 2, 1, 8],
-            [11, -4, 2, 2, 2, 2, -4, 11],
-            [-3, -7, -4, 1, 1, -4, -7, -3],
-            [20, -3, 11, 8, 8, 11, -3, 20]
+            [4, -3, 2, 2, 2, 2, -3, 4],
+            [-3, -4, -1, -1, -1, -1, -4, -3],
+            [2, -1, 1, 0, 0, 1, -1, 2],
+            [2, -1, 0, 1, 1, 0, -1, 2],
+            [2, -1, 0, 1, 1, 0, -1, 2],
+            [2, -1, 1, 0, 0, 1, -1, 2],
+            [-3, -4, -1, -1, -1, -1, -4, -3],
+            [4, -3, 2, 2, 2, 2, -3, 4],
         ]
 
         # =============================================================================================
@@ -147,20 +157,25 @@ class ReversiAI():
         # =============================================================================================
         # 1 - Coin Parity
         # =============================================================================================
-        if my_tiles > opp_tiles:
-            p = (100.0 * my_tiles) / (my_tiles + opp_tiles)
-        elif my_tiles < opp_tiles:
-            p = -(100.0 * opp_tiles) / (my_tiles + opp_tiles)
-        else:
-            p = 0
+        # if my_tiles > opp_tiles:
+        #     p = (100.0 * my_tiles) / (my_tiles + opp_tiles)
+        # elif my_tiles < opp_tiles:
+        #     p = -(100.0 * opp_tiles) / (my_tiles + opp_tiles)
+        # else:
+        #     p = 0
+        p = 100 * (my_tiles - opp_tiles) / (my_tiles + opp_tiles)
 
         # =============================================================================================
         # 2 - Stability - calculates the blank Spaces around my tiles
         # =============================================================================================
-        if my_front_tiles > opp_front_tiles:
-            f = -(100.0 * my_front_tiles) / (my_front_tiles + opp_front_tiles)
-        elif my_front_tiles < opp_front_tiles:
-            f = (100.0 * opp_front_tiles) / (my_front_tiles + opp_front_tiles)
+        # if my_front_tiles > opp_front_tiles:
+        #     f = -(100.0 * my_front_tiles) / (my_front_tiles + opp_front_tiles)
+        # elif my_front_tiles < opp_front_tiles:
+        #     f = (100.0 * opp_front_tiles) / (my_front_tiles + opp_front_tiles)
+        # else:
+        #     f = 0
+        if (my_front_tiles + opp_front_tiles != 0):
+            f = -(100.0 * (my_front_tiles - opp_front_tiles)) / (my_front_tiles + opp_front_tiles)
         else:
             f = 0
 
@@ -189,7 +204,11 @@ class ReversiAI():
             my_tiles += 1
         elif board[7][7] == opp_color:
             opp_tiles += 1
-        c = 25 * (my_tiles - opp_tiles)
+        # c = 25 * (my_tiles - opp_tiles)
+        if (my_tiles + opp_tiles != 0):
+            c = 100 * (my_tiles - opp_tiles) / (my_tiles + opp_tiles)
+        else:
+            c = 0
 
         # ===============================================================================================
         # 4 - CORNER CLOSENESS
@@ -256,7 +275,11 @@ class ReversiAI():
             elif board[7][6] == opp_color:
                 opp_tiles += 1
 
-        l = -12.5 * (my_tiles - opp_tiles)
+        # l = -12.5 * (my_tiles - opp_tiles)
+        if (my_tiles + opp_tiles != 0):
+            l = -100 * (my_tiles - opp_tiles) / (my_tiles + opp_tiles)
+        else:
+            l = 0
 
         # ===============================================================================================
         # 5 - Mobility
@@ -271,10 +294,14 @@ class ReversiAI():
         my_tiles = len(AIHelper().available_moves(board, my_color))
         opp_tiles = len(AIHelper().available_moves(board, opp_color))
 
-        if my_tiles > opp_tiles:
-            m = (100.0 * my_tiles) / (my_tiles + opp_tiles)
-        elif my_tiles < opp_tiles:
-            m = -(100.0 * opp_tiles) / (my_tiles + opp_tiles)
+        # if my_tiles > opp_tiles:
+        #     m = (100.0 * my_tiles) / (my_tiles + opp_tiles)
+        # elif my_tiles < opp_tiles:
+        #     m = -(100.0 * opp_tiles) / (my_tiles + opp_tiles)
+        # else:
+        #     m = 0
+        if (my_tiles + opp_tiles != 0): 
+            m = (100.0 * (my_tiles - opp_tiles)) / (my_tiles + opp_tiles)
         else:
             m = 0
 
@@ -285,14 +312,12 @@ class ReversiAI():
         if self.heuristic == 1:
             return p
         elif self.heuristic == 2:
-            return f
+            return f + l
         elif self.heuristic == 3:
             return c
         elif self.heuristic == 4:
-            return l
-        elif self.heuristic == 5:
             return m
-        elif self.heuristic == 6:
+        elif self.heuristic == 5:
             return d
         else:
             return (10 * p) + (801.724 * c) + (382.026 * l) + (78.922 * m) + (74.396 * f) + (10 * d)
